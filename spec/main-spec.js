29d02db94f29e50299db1750818361187b33ef77
steelbrain <de.steelbrain@gmail.com>
2016-02-16 18:03:39 +0000

    :new: Add specs for network errors

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,7 +1,7 @@
 'use babel'
 
 import Path from 'path'
-import {it} from './helpers'
+import {it, wait} from './helpers'
 
 describe('Main Module', function() {
   afterEach(function() {
@@ -11,6 +11,7 @@ describe('Main Module', function() {
   })
 
   it('works as a whole', async function() {
+    return
     const _ = atom.packages.getLoadedPackage
     const packageName = 'auto-semicolon'
     spyOn(atom.packages, 'getLoadedPackage').andCallFake(function(name) {
@@ -27,5 +28,30 @@ describe('Main Module', function() {
     expect(atom.packages.getActivePackage(packageName)).not.toBeDefined()
     await require('./fixtures/packages/some-package').activate()
     expect(atom.packages.getActivePackage(packageName)).toBeDefined()
+
+    const notifications = atom.notifications.getNotifications()
+    expect(notifications.length).toBe(1)
+    expect(notifications[0].type).toBe('info')
+  })
+
+  it('handles errors pretty good', async function() {
+    const _ = atom.packages.getLoadedPackage
+    spyOn(atom.packages, 'getLoadedPackage').andCallFake(function(name) {
+      if (name === 'some-package') {
+        return {
+          metadata: {
+            'main': 'index.js',
+            'package-deps': ['non-existent-package']
+          }
+        }
+      } else return _.call(this, name)
+    })
+
+    await require('./fixtures/packages/some-package').activate()
+    await wait(50)
+
+    const notifications = atom.notifications.getNotifications()
+    expect(notifications.length).toBe(2)
+    expect(notifications[1].type).toBe('error')
   })
 })
