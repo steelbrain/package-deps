dc3baeebe3555f2d05a36a8c101af1d57bd41b8b
steelbrain <de.steelbrain@gmail.com>
2016-02-16 17:21:38 +0000

    :new: Add Installer class

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 'use strict'
 
+import {View} from './view'
+import {spawnAPM} from './helpers'
+
 export class Installer {
-  constructor(packages) {
+  constructor(name, packages) {
+    this.name = name
     this.packages = packages
   }
   async install() {
-    console.log('installer', this.packages)
+    const view = new View(this.name, this.packages)
+    try {
+      await spawnAPM(this.packages, function() {
+        view.advance()
+      })
+    } catch (error) {
+      view.dismiss()
+      atom.notifications.addError(`Error installing ${this.name} dependencies`, {
+        detail: error.stack,
+        dismissable: true
+      })
+    } finally {
+      await Promise.all(this.packages.map(name => atom.packages.activatePackage(name)))
+    }
   }
 }
