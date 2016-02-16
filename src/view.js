a30a549c693b1f343dc2ee664e00f5117f95afce
steelbrain <de.steelbrain@gmail.com>
2016-02-16 16:47:44 +0000

    :new: Rewrite the view class

--- src/view.js
+++ src/view.js
@@ -3,50 +3,45 @@
 export class View {
   constructor(name, dependencies) {
     this.name = name
+    this.progress = document.createElement('progress')
+    this.completed = 0
     this.dependencies = dependencies
-
-    const progress = this.progress = document.createElement('progress')
-    progress.max = dependencies.length
-    progress.value = 0
-    progress.classList.add('display-inline')
-    progress.style.width = '100%'
-
-    this.notification = this.element = null
-  }
-  show() {
-    this.notification = atom.notifications.addInfo(`Installing ${this.name} dependencies`, {
-      detail: `Installing ${this.dependencies.join(', ')}`,
+    this.notification = atom.notifications.addInfo(`Installing ${name} dependencies`, {
+      detail: `Installing ${dependencies.join(', ')}`,
       dismissable: true
     })
-    this.element = document.createElement('div') // placeholder
-    setTimeout(() => {
-      try {
-        this.element = atom.views.getView(this.notification)
+    this.notificationView = atom.views.getView(this.notification)
 
-        const content = this.element.querySelector('.detail-content')
-        if (content) {
-          content.appendChild(this.progress)
-        }
-      } catch (_) { }
-    }, 20)
+    const content = this.notificationView.querySelector('.detail-content')
+    if (content) {
+      content.appendChild(this.progress)
+    }
+    this.progress.max = dependencies.length
+    this.progress.style.width = '100%'
   }
   advance() {
-    this.progress.value++
-    if (this.progress.value === this.progress.max) {
-      const content = this.element.querySelector('.detail-content')
-      const title = this.element.querySelector('.message p')
-
-      if (content) {
-        content.textContent = `Installed ${this.dependencies.join(', ')}`
-      }
-      if (title) {
-        title.textContent = `Installed ${this.name} dependencies`
-      }
-
-      this.element.classList.remove('info')
-      this.element.classList.remove('icon-info')
-      this.element.classList.add('success')
-      this.element.classList.add('icon-check')
+    this.completed++
+    if (this.completed > 1) {
+      this.progress.value = this.completed
     }
+  }
+  markFinished() {
+    const content = this.notificationView.querySelector('.detail-content')
+    const title = this.notificationView.querySelector('.message p')
+
+    if (content) {
+      content.textContent = `Installed ${this.dependencies.join(', ')}`
+    }
+    if (title) {
+      title.textContent = `Installed ${this.name} dependencies`
+    }
+
+    this.notificationView.classList.remove('info')
+    this.notificationView.classList.remove('icon-info')
+    this.notificationView.classList.add('success')
+    this.notificationView.classList.add('icon-check')
+  }
+  dismiss() {
+    this.notification.dismiss()
   }
 }
