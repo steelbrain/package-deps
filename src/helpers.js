64be6b39b2d118959b68afce0ba469d218b37886
Joel Barker <joel@mojamail.com>
2016-08-08 21:34:36 -0700

    install dependencies in parallel

--- src/helpers.js
+++ src/helpers.js
@@ -5,37 +5,39 @@ import {View} from './view'
 const extractionRegex = /(?:Installing|Moving) (.*?) to .* (.*)/
 
 export function spawnAPM(dependencies, progressCallback) {
-  return new Promise(function(resolve, reject) {
-    const errors = []
-    let successes = 0
-    new BufferedProcess({
-      command: atom.packages.getApmPath(),
-      args: ['install'].concat(dependencies).concat(['--production', '--color', 'false']),
-      options: {},
-      stdout: function(contents) {
-        const matches = extractionRegex.exec(contents)
-        if (!matches) {
-          // info messages: ignore
-        } else if (matches[2] === '✓' || matches[2] === 'done') {
-          progressCallback(matches[1], true)
-          successes++
-        } else {
-          progressCallback(matches[1], false)
-          errors.push(matches[1])
+  return Promise.all(dependencies.map(function(dependency) {
+    return new Promise(function(resolve, reject) {
+      const errors = []
+      let successes = 0
+      new BufferedProcess({
+        command: atom.packages.getApmPath(),
+        args: ['install'].concat([dependency, '--production', '--color', 'false']),
+        options: {},
+        stdout: function(contents) {
+          const matches = extractionRegex.exec(contents)
+          if (!matches) {
+            // info messages: ignore
+          } else if (matches[2] === '✓' || matches[2] === 'done') {
+            progressCallback(matches[1], true)
+            successes++
+          } else {
+            progressCallback(matches[1], false)
+            errors.push(matches[1])
+          }
+        },
+        stderr: function(contents) {
+          errors.push(contents)
+        },
+        exit: function() {
+          if (successes !== dependencies.length) {
+            const error = new Error('Error installing dependency: ' + dependency)
+            error.stack = errors.join('\n')
+            reject(error)
+          } else resolve()
         }
-      },
-      stderr: function(contents) {
-        errors.push(contents)
-      },
-      exit: function() {
-        if (successes !== dependencies.length) {
-          const error = new Error('Error installing dependencies')
-          error.stack = errors.join('\n')
-          reject(error)
-        } else resolve()
-      }
+      })
     })
-  })
+  }))
 }
 
 export async function installDependencies(name, packages) {
