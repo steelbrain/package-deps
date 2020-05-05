c10a4a9ae754e14b5280998f382794a033d9a0ce
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-05-05 14:05:58 -0500

    rollup config

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import peerexternal from 'rollup-plugin-peer-deps-external'
+// import nodePolyfills from 'rollup-plugin-node-polyfills'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+
+const plugins = [
+  babel(),
+
+  // nodePolyfills(), // bundle path, fs
+
+  peerexternal({
+    includeDependencies: false,
+  }),
+
+  // so Rollup can find externals
+  resolve({ extensions: ['.js'], preferBuiltins: true }),
+
+  // so Rollup can convert externals to an ES module
+  commonjs(),
+]
+
+// minify only in production mode
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(
+    // minify
+    terser({
+      ecma: 2018,
+      warnings: true,
+      compress: {
+        drop_console: true,
+      },
+    }),
+  )
+}
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        dir: 'lib',
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    // loaded externally
+    external: ['atom'],
+    plugins: plugins,
+  },
+]
