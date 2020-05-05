f52220fa9e4cdf4830fdc653b087154556855a91
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-05-05 14:07:56 -0500

    dynamic import

--- src/index.js
+++ src/index.js
@@ -7,34 +7,35 @@ if (typeof window.__steelbrain_package_deps === 'undefined') {
   window.__steelbrain_package_deps = new Set()
 }
 
-async function installDependencies(packageName: string, promptUser: boolean = true): Promise<void> {
+async function installDependencies(packageName: string, shouldPromptUser: boolean = true): Promise<void> {
   invariant(packageName, '[Package-Deps] Package name is required')
 
-  const dependencies = await Helpers.getDependencies(packageName)
+  const dependencies = await getDependencies(packageName)
   if (!dependencies.length) {
     return
   }
 
-  if (promptUser) {
-    const choice = await Helpers.promptUser(packageName, dependencies)
+  // Prompt user
+  if (shouldPromptUser) {
+    let choice
+    import('./prompt').then(async ({ promptUser }) => {
+      choice = await promptUser(packageName, dependencies)
+    })
+
     if (choice !== 'Yes') {
       return
     }
   }
 
-  const view = new View(packageName, dependencies)
-  const errors = await Helpers.apmInstall(dependencies, function() {
-    view.advance()
+  let promises
+  import('./install').then(async ({ performInstall }) => {
+    // Perform Installation
+    promises = await performInstall(packageName, dependencies)
   })
-  const promises = []
-  view.complete(errors)
-  for (const dependency of (dependencies: Array<Dependency>)) {
-    if (errors.has(dependency.name)) {
-      continue
-    }
-    promises.push(atom.packages.activatePackage(dependency.name))
-  }
+
   await Promise.all(promises)
 }
 
 export { installDependencies as install }
+
+// dynamic import doc: https://rollupjs.org/guide/en/#dynamic-import
